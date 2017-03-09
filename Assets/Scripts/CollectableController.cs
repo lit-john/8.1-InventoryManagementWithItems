5b7ff9aa4cf23aefefda2d552a37a596fd1e0f9e
lit-john <john.hannafin@lit.ie>
2017-03-09 13:40:22 +0000

    Initial Code

---
+++ Assets/Scripts/CollectableController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* 
+ * This script could be attached to any GameObject that is a collectable. The script notifies the LevelManager when the
+ * items trigger box is entered and exited. The Levelmanager notifies the collectable when it is collected by calling the
+ * function OnCollected.
+ * 
+ * When notifying the LevelManager that this Collectable's trigger box has been entered/exited, the CollectableController
+ * calls the function OnCollectableTriggerEnter/OnCollectableTriggerExit on the LevelManager. Both these functions take
+ * two arguments, the first is the variable 'this' which is a reference to "this CollectableController component" (remember there can
+ * be many CollectableController components (objects) in the scene i.e. one attached to each collectable game object), and the
+ * second argument is the Collider2D object that Unity passes into the OnTriggerEnter2D/OnTriggerEnter2D function. The
+ * LevelManager can use these arguments to determine what game object triggered what collectable.
+ * 
+ */
+public class CollectableController : MonoBehaviour {
+	public LevelManager theLevelManager;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		theLevelManager.OnCollectableTriggerEnter (this, other);
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		theLevelManager.OnCollectableTriggerExit (this, other);
+	}
+
+	public void OnCollected() {
+		// What do we want to do when collected. For prototyping purposes I'm simple disable the
+		// SpriteRender and the Trigger box
+
+		// Get the SpriteRender component
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+
+		// Set enable to false
+		sr.enabled = false;
+
+		BoxCollider2D bc = GetComponent<BoxCollider2D> ();
+		bc.enabled = false;
+	}
+}
---
+++ Assets/Scripts/InventoryUIManager.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/*
+ * This script is attached to the InventoryPanel. It is responsible for updating the inventory UI
+ */
+public class InventoryUIManager : MonoBehaviour {
+	/* 
+	 * Each inventory item has:
+	 * 	- an image
+	 *  - a piece ot text (containing a number that represents how many of this item has been collected)
+	 *  - a number (count) that represents how many of this item has been collected
+	 * 
+	 * I have created a UI Panel for each item. The variables below point to the appropriate component
+	 * within the appropriate UI Panel. These need to be set up via the inspector.
+	 */
+	public Image cheeseImage;
+	public Image orangeImage;
+	public Image appleImage;
+
+	public Text cheeseText;
+	public Text orangeText;
+	public Text appleText;
+
+	/* 
+	 * The following variables keep track of the, count, number of collectables collected. I will later 
+	 * convert these numbers to strings to display in the Text conpmonent. I'm going to initialise
+	 * tham all to 0.
+	 */
+	private int cheeseCount = 0;
+	private int orangeCount = 0;
+	private int appleCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		// Disable all the images so tha twe don;t see anything
+		cheeseImage.enabled = false;
+		orangeImage.enabled = false;
+		appleImage.enabled = false;
+
+		// Set the text to blank
+		cheeseText.text = orangeText.text = appleText.text = "";
+	}
+
+	/*
+	 * All of the following add??? methods follow the same format. Increase the count of the collectible
+	 * by one, if the count is equal to 1 enable the image so that we can see it, convert the count number to 
+	 * text and assign it to the text component of the UI.
+	 */
+	public void addApple() {
+		appleCount++;
+
+		if (appleCount == 1) {
+			appleImage.enabled = true;
+		}
+
+		appleText.text = appleCount.ToString ();
+	}
+
+	/*
+	 * All of the remove??? functions also follow the same format. Decrement the count of the collectable,
+	 * if the count is 0 hide the image, update the text component to match the count.
+	 */
+	public void removeApple() {
+		appleCount--;
+		appleText.text = appleCount.ToString ();
+
+		if (appleCount == 0) {
+			appleImage.enabled = false;
+		}
+	}
+
+	public void addCheese() {
+		cheeseCount++;
+
+		if (cheeseCount == 1) {
+			cheeseImage.enabled = true;
+		}
+
+		cheeseText.text = cheeseCount.ToString ();
+	}
+
+	public void removeCheese() {
+		cheeseCount--;
+		cheeseText.text = cheeseCount.ToString ();
+
+		if (cheeseCount == 0) {
+			cheeseImage.enabled = false;
+		}
+	}
+
+	public void addOrange() {
+		orangeCount++;
+
+		if (orangeCount == 1) {
+			orangeImage.enabled = true;
+		}
+
+		orangeText.text = orangeCount.ToString ();
+	}
+
+	public void removeOrange() {
+		orangeCount--;
+		orangeText.text = orangeCount.ToString ();
+
+		if (orangeCount == 0) {
+			orangeImage.enabled = false;
+		}
+	}
+}
